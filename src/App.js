6631ba511c0d4304e18e8d0d218b29039e6bc84f
shivshankar shejwal <shejwal1shivshankar@gmail.com>
2019-07-06 14:17:26 +0530

    Adding  git ignore , eslint and prettier configurations and files.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-const Pet = ({
-    name,
-    animal,
-    breed
-}) =>{
-    return React.createElement("div",{},[
-        React.createElement("h1",{},name),
-        React.createElement("h2",{},animal),
-        React.createElement("h2",{},breed)
-    ]);
-}
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
 
-const App  = () =>{
-    return React.createElement("div",{},[
-        React.createElement("h1",{},"Adopt Me !"),
-        React.createElement(Pet,{name:"Luna",animal:"Doggy",breed:"Havanese"}),
-        React.createElement(Pet,{name:"pepper",animal:"Dog",breed:"indian-dog"}),
-        React.createElement(Pet,{name:"maggie",animal:"Cat",breed:"breeze"})
-    ]);
-}
-ReactDOM.render(React.createElement(App),document.getElementById("root"));
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me !"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "pepper",
+      animal: "Dog",
+      breed: "indian-dog"
+    }),
+    React.createElement(Pet, { name: "maggie", animal: "Cat", breed: "breeze" })
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
